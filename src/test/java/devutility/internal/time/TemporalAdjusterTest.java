42e8fa6898ff961353368b6ad86f3a47de636c7b
eagle6688 <fangyansu@163.com>
2017-07-02 21:28:50 +0800

    Update

---
+++ src/test/java/devutility/internal/time/TemporalAdjusterTest.java
@@ -0,0 +1,22 @@
+package devutility.internal.time;
+
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjuster;
+
+public class TemporalAdjusterTest {
+
+	static TemporalAdjuster NEXT_WORKDAY = w -> {
+		LocalDate result = (LocalDate) w;
+
+		do {
+			result = result.plusDays(1);
+		} while (result.getDayOfWeek().getValue() >= 6);
+
+		return result;
+	};
+
+	public static void main(String[] args) {
+		LocalDate backToWork = LocalDate.now().with(NEXT_WORKDAY);
+		System.out.println(backToWork.toString());
+	}
+}
