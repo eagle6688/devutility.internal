041137090f96db9bd4d87be58b920490c7307a0d
su liang <fangyansu@163.com>
2017-10-24 17:05:54 +0800

    Update

--- src/main/java/devutility/internal/system/Application.java
+++ src/main/java/devutility/internal/system/Application.java
@@ -3,11 +3,12 @@ package devutility.internal.system;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class Application {
-	//region get property
-	
+	// region get property
+
 	public static String getProperty(String path, String key) throws FileNotFoundException, IOException {
 		try (FileInputStream fileInputStream = new FileInputStream(path)) {
 			Properties properties = new Properties();
@@ -17,15 +18,31 @@ public class Application {
 			throw e;
 		}
 	}
-	
-	//endregion
 
-	//region get int property
-	
+	public static String getPropertyByResourceName(String name, String key) {
+		try (InputStream inputStream = Application.class.getClassLoader().getResourceAsStream(name)) {
+			Properties properties = new Properties();
+			properties.load(inputStream);
+			return properties.getProperty(key);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	// endregion
+
+	// region get int property
+
 	public static int getIntProperty(String path, String key) throws FileNotFoundException, IOException {
 		String value = getProperty(path, key);
 		return Integer.parseInt(value);
 	}
-	
-	//endregion
+
+	public static int getIntPropertyByResourceName(String name, String key) {
+		String value = getPropertyByResourceName(name, key);
+		return Integer.parseInt(value);
+	}
+
+	// endregion
 }
