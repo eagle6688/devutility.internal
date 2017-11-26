af9f07114548e7b38a0031b4cfa5aa74bb4451ca
Eagle <Eagle@10.96.192.174>
2017-11-26 19:09:26 +0800

    Add getDaySubtract

--- src/main/java/devutility/internal/util/DateHelper.java
+++ src/main/java/devutility/internal/util/DateHelper.java
@@ -1,7 +1,13 @@
 package devutility.internal.util;
 
+import java.util.Date;
+
 public class DateHelper {
 	public static int millisToDays(long millis) {
 		return (int) (millis / (1000 * 60 * 60 * 24));
+	}
+
+	public static int getDaySubtract(Date d1, Date d2) {
+		return (int) ((d1.getTime() - d2.getTime()) / (24 * 60 * 60 * 1000));
 	}
 }
