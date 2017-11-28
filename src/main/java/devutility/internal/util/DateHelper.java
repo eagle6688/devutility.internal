767abfac6c772bc8deef7711cec8bf889f6c5e10
Eagle <Eagle@10.96.204.113>
2017-11-28 20:56:16 +0800

    Update DateHelper.java

--- src/main/java/devutility/internal/util/DateHelper.java
+++ src/main/java/devutility/internal/util/DateHelper.java
@@ -1,6 +1,9 @@
 package devutility.internal.util;
 
+import java.text.ParseException;
 import java.util.Date;
+
+import devutility.internal.text.format.DateFormatHelper;
 
 public class DateHelper {
 	public static int millisToDays(long millis) {
@@ -13,5 +16,14 @@ public class DateHelper {
 
 	public static int getHoursSubtract(Date d1, Date d2) {
 		return (int) ((d1.getTime() - d2.getTime()) / (60 * 60 * 1000));
+	}
+
+	public static Date toDate(String value, String pattern) {
+		try {
+			return DateFormatHelper.toDate(value, pattern);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
