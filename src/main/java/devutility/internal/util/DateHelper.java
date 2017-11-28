b2cc26ac6c5c61654fcd9486eafdc32ec9a880aa
su liang <fangyansu@163.com>
2017-11-28 11:53:52 +0800

    Update

--- src/main/java/devutility/internal/util/DateHelper.java
+++ src/main/java/devutility/internal/util/DateHelper.java
@@ -10,4 +10,8 @@ public class DateHelper {
 	public static int getDaySubtract(Date d1, Date d2) {
 		return (int) ((d1.getTime() - d2.getTime()) / (24 * 60 * 60 * 1000));
 	}
+
+	public static int getHoursSubtract(Date d1, Date d2) {
+		return (int) ((d1.getTime() - d2.getTime()) / (60 * 60 * 1000));
+	}
 }
