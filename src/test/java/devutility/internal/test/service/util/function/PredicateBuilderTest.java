8929799a93b2d37c663ec278f44710b79fc98b1c
su liang <fangyansu@163.com>
2017-12-01 11:06:44 +0800

    Test

--- src/test/java/devutility/internal/test/service/util/function/PredicateBuilderTest.java
+++ src/test/java/devutility/internal/test/service/util/function/PredicateBuilderTest.java
@@ -1,16 +1,19 @@
 package devutility.internal.test.service.util.function;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import devutility.internal.test.BaseService;
 import devutility.internal.test.ServiceExecutor;
+import devutility.internal.test.models.Person;
 import devutility.internal.util.ListHelper;
 import devutility.internal.util.function.PredicateBuilder;
 
 public class PredicateBuilderTest extends BaseService {
 	@Override
 	public void run() {
+		testOrAnd();
 		ArrayList<Integer> list = new ArrayList<>();
 		list.add(1);
 		list.add(3);
@@ -31,6 +34,26 @@ public class PredicateBuilderTest extends BaseService {
 		int maxValue = ListHelper.maxInt(list);
 		println("maxValue");
 		println(maxValue);
+	}
+
+	private void testOrAnd() {
+		List<Person> list = new ArrayList<>();
+		list.add(new Person("James", 30));
+		list.add(new Person("Susan", 30));
+		list.add(new Person("Aldwin", 29));
+		list.add(new Person("Pete", 50));
+		list.add(new Person("Hoken", 40));
+
+		PredicateBuilder<Person> predicateBuilder1 = new PredicateBuilder<Person>();
+		PredicateBuilder<Person> predicateBuilder2 = new PredicateBuilder<Person>();
+
+		for (String name : Arrays.asList("s", "win")) {
+			predicateBuilder2.or(i -> i.getName() != null && i.getName().toLowerCase().indexOf(name.toLowerCase()) > -1);
+		}
+
+		predicateBuilder1.and(i -> i.getAge() == 30);
+		List<Person> result = ListHelper.list(list, predicateBuilder1.getPredicate());
+		println(result.size());
 	}
 
 	public static void main(String[] args) {
