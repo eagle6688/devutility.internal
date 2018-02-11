8b446bf2b57597c73cf8d4b34f95ca498702f00b
Aldwin Su <fangyansu@163.com>
2018-02-11 15:45:37 +0800

    Update SearchResult

--- src/main/java/devutility/internal/models/SearchResult.java
+++ src/main/java/devutility/internal/models/SearchResult.java
@@ -3,36 +3,22 @@ package devutility.internal.models;
 import java.util.List;
 
 public class SearchResult<T> {
-	private boolean changed;
-	private List<T> list;
+	private int count;
+	private List<T> data;
 
-	public SearchResult() {
-		changed = false;
+	public int getCount() {
+		return count;
 	}
 
-	public SearchResult(List<T> list) {
-		this();
-		this.list = list;
+	public void setCount(int count) {
+		this.count = count;
 	}
 
-	public boolean isChanged() {
-		return changed;
+	public List<T> getData() {
+		return data;
 	}
 
-	public void setChanged(boolean changed) {
-		this.changed = changed;
-	}
-
-	public List<T> getList() {
-		return list;
-	}
-
-	public void setList(List<T> list) {
-		this.list = list;
-	}
-
-	public void setChangedList(List<T> list) {
-		setChanged(true);
-		setList(list);
+	public void setData(List<T> data) {
+		this.data = data;
 	}
 }
