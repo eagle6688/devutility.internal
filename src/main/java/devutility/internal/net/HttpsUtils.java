65d76d5b389b01492fafa1682094bc4f61eb07c5
Aldwin Su <fangyansu@163.com>
2018-04-26 23:13:10 +0800

    Add HttpsUtils

--- src/main/java/devutility/internal/net/HttpsUtils.java
+++ src/main/java/devutility/internal/net/HttpsUtils.java
@@ -1,6 +1,7 @@
 package devutility.internal.net;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
@@ -10,10 +11,43 @@ import javax.net.ssl.KeyManager;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
 
+import devutility.internal.data.codec.UTF8Utils;
+import devutility.internal.io.StreamHelper;
 import devutility.internal.lang.StringHelper;
 
 public class HttpsUtils {
-	private static HttpsURLConnection httpsURLConnection(URL url, String method, String contentType, byte[] data, SSLSocketFactory sslSocketFactory) throws IOException {
+	public static String get(String url, String protocol) throws KeyManagementException, NoSuchAlgorithmException, IOException {
+		return get(url, protocol, 0);
+	}
+
+	public static String get(String url, String protocol, int timeout) throws IOException, KeyManagementException, NoSuchAlgorithmException {
+		return get(url, null, protocol, timeout);
+	}
+
+	public static String get(String url, String contentType, String protocol, int timeout) throws IOException, KeyManagementException, NoSuchAlgorithmException {
+		byte[] bytes = null;
+		HttpsURLConnection httpsURLConnection = httpsURLConnection(url, "GET", contentType, protocol, timeout);
+
+		try (InputStream inputStream = httpsURLConnection.getInputStream()) {
+			bytes = StreamHelper.read(inputStream);
+		} catch (IOException e) {
+			throw e;
+		}
+
+		return UTF8Utils.decode(bytes);
+	}
+
+	private static HttpsURLConnection httpsURLConnection(String url, String method, String contentType, String protocol, int timeout) throws KeyManagementException, NoSuchAlgorithmException, IOException {
+		return httpsURLConnection(url, method, contentType, null, protocol, timeout);
+	}
+
+	private static HttpsURLConnection httpsURLConnection(String url, String method, String contentType, byte[] data, String protocol, int timeout) throws IOException, KeyManagementException, NoSuchAlgorithmException {
+		URL urlObj = new URL(url);
+		SSLSocketFactory sslSocketFactory = sslSocketFactory(protocol);
+		return httpsURLConnection(urlObj, method, contentType, data, sslSocketFactory, timeout);
+	}
+
+	private static HttpsURLConnection httpsURLConnection(URL url, String method, String contentType, byte[] data, SSLSocketFactory sslSocketFactory, int timeout) throws IOException {
 		HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
 		httpsURLConnection.setUseCaches(false);
 		httpsURLConnection.setRequestProperty("charset", "utf-8");
@@ -28,7 +62,17 @@ public class HttpsUtils {
 			httpsURLConnection.setRequestProperty("content-Type", contentType);
 		}
 
-		return null;
+		if (method.equals("POST") && data != null) {
+			httpsURLConnection.setDoOutput(true);
+			httpsURLConnection.setRequestProperty("Content-length", String.valueOf(data.length));
+		}
+
+		if (timeout > 0) {
+			httpsURLConnection.setConnectTimeout(timeout);
+			httpsURLConnection.setReadTimeout(timeout);
+		}
+
+		return httpsURLConnection;
 	}
 
 	private static SSLSocketFactory sslSocketFactory(String protocol) throws NoSuchAlgorithmException, KeyManagementException {
---
+++ src/test/java/devutility/internal/test/service/net/httpsutils/GetTest.java
@@ -0,0 +1,24 @@
+package devutility.internal.test.service.net.httpsutils;
+
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+import devutility.internal.net.HttpsUtils;
+import devutility.internal.test.BaseTest;
+import devutility.internal.test.TestExecutor;
+
+public class GetTest extends BaseTest {
+	@Override
+	public void run() {
+		try {
+			println(HttpsUtils.get("https://www.baidu.com", "TLS"));
+		} catch (KeyManagementException | NoSuchAlgorithmException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		TestExecutor.run(GetTest.class);
+	}
+}
