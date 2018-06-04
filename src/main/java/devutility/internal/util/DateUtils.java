16841c515e075652a6a5cb802c9400c01a47c046
Aldwin Su <fangyansu@163.com>
2018-06-04 14:02:53 +0800

    Update DateUtils.java

--- src/main/java/devutility/internal/util/DateUtils.java
+++ src/main/java/devutility/internal/util/DateUtils.java
@@ -6,25 +6,21 @@ public class DateUtils {
 	/**
 	 * Milliseconds to days
 	 * @param milliseconds: Milliseconds
-	 * @return long
+	 * @return double
 	 */
-	public static long millisecondsToDays(long milliseconds) {
+	public static double millisecondsToDays(long milliseconds) {
 		long dividend = 1000 * 60 * 60 * 24;
-		long mod = milliseconds % dividend;
-		long quotient = milliseconds / dividend;
-		return mod == 0 ? quotient : quotient + 1;
+		return milliseconds / dividend;
 	}
 
 	/**
 	 * Milliseconds to hours
 	 * @param milliseconds: Milliseconds
-	 * @return long
+	 * @return double
 	 */
-	public static long millisecondsToHours(long milliseconds) {
+	public static double millisecondsToHours(long milliseconds) {
 		long dividend = 1000 * 60 * 60;
-		long mod = milliseconds % dividend;
-		long quotient = milliseconds / dividend;
-		return mod == 0 ? quotient : quotient + 1;
+		return milliseconds / dividend;
 	}
 
 	/**
@@ -44,6 +40,16 @@ public class DateUtils {
 	 * @return long
 	 */
 	public static long subtractToLongDays(Date d1, Date d2) {
+		return (long) millisecondsToDays(d1.getTime() - d2.getTime());
+	}
+
+	/**
+	 * Subtract two dates and return days with double type.
+	 * @param d1: First date
+	 * @param d2: Second date
+	 * @return double
+	 */
+	public static double substractToDoubleDays(Date d1, Date d2) {
 		return millisecondsToDays(d1.getTime() - d2.getTime());
 	}
 
@@ -51,9 +57,9 @@ public class DateUtils {
 	 * Subtract two dates in hours
 	 * @param d1: First date
 	 * @param d2: Second date
-	 * @return long
+	 * @return double
 	 */
-	public static long subtractToHours(Date d1, Date d2) {
+	public static double subtractToHours(Date d1, Date d2) {
 		return millisecondsToHours(d1.getTime() - d2.getTime());
 	}
 }
