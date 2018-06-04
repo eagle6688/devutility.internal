40ffdf26f27e133af51c09f351c9c956f7193958
Aldwin Su <fangyansu@163.com>
2018-06-04 23:24:44 +0800

    Add SingletonListTest

---
+++ src/test/java/devutility/internal/test/service/basic/util/collections/SingletonListTest.java
@@ -0,0 +1,19 @@
+package devutility.internal.test.service.basic.util.collections;
+
+import java.util.Collections;
+import java.util.List;
+
+import devutility.internal.test.BaseTest;
+import devutility.internal.test.TestExecutor;
+
+public class SingletonListTest extends BaseTest {
+	@Override
+	public void run() {
+		List<String> list = Collections.singletonList("asd");
+		println(list.toString());
+	}
+
+	public static void main(String[] args) {
+		TestExecutor.run(SingletonListTest.class);
+	}
+}
