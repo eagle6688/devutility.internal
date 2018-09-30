af81c1a928c9b597b70033356e340165d4bd9e2c
Aldwin Su <fangyansu@163.com>
2018-09-30 21:22:03 +0800

    Add ChannelUtils

---
+++ src/main/java/devutility/internal/nio/ChannelUtils.java
@@ -0,0 +1,35 @@
+package devutility.internal.nio;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.ReadableByteChannel;
+
+public class ChannelUtils {
+	/**
+	 * Read bytes from ReadableByteChannel object.
+	 * @param channel: ReadableByteChannel object.
+	 * @param bufferSize: Buffer size.
+	 * @return byte[]
+	 */
+	public static byte[] read(ReadableByteChannel channel, int bufferSize) {
+		int readBytesCount = 0;
+		ByteBuffer byteBuffer = ByteBuffer.allocate(bufferSize);
+
+		try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream()) {
+			while ((readBytesCount = channel.read(byteBuffer)) > 0) {
+				byteBuffer.flip();
+
+				if (byteBuffer.hasArray()) {
+					byteArrayOutputStream.write(byteBuffer.array(), 0, readBytesCount);
+				}
+
+				byteBuffer.clear();
+			}
+
+			return byteArrayOutputStream.toByteArray();
+		} catch (Exception e) {
+			System.out.println(e.toString());
+			return null;
+		}
+	}
+}
