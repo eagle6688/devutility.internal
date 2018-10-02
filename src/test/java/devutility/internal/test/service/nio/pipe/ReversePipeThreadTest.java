b49f17012df2195959be0d1513a2c90a093ad14b
Aldwin Su <fangyansu@163.com>
2018-10-02 17:12:56 +0800

    Update ReversePipeThreadTest

--- src/test/java/devutility/internal/test/service/nio/pipe/ReversePipeThreadTest.java
+++ src/test/java/devutility/internal/test/service/nio/pipe/ReversePipeThreadTest.java
@@ -20,11 +20,7 @@ public class ReversePipeThreadTest extends BaseTest {
 		reversePipeThread.start();
 
 		while (true) {
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+			Thread.yield();
 		}
 	}
 
