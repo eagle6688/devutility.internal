dd7a0e7434f26589dc27f0f03208b19c9e15b12e
Aldwin Su <fangyansu@163.com>
2018-11-22 15:27:18 +0800

    Add UuidTest

---
+++ src/test/java/devutility/internal/test/service/util/uuid/UuidTest.java
@@ -0,0 +1,17 @@
+package devutility.internal.test.service.util.uuid;
+
+import java.util.UUID;
+
+import devutility.internal.test.BaseTest;
+import devutility.internal.test.TestExecutor;
+
+public class UuidTest extends BaseTest {
+	@Override
+	public void run() {
+		println(UUID.randomUUID().toString());
+	}
+
+	public static void main(String[] args) {
+		TestExecutor.run(UuidTest.class);
+	}
+}
