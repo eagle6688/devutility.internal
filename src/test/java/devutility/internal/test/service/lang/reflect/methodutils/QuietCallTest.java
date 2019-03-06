198f2758b239d51880190642d4cf9acc372b6e7c
Aldwin Su <fangyansu@163.com>
2019-03-06 14:46:25 +0800

    Add a test case for quietCall method in MethodUtils.java

---
+++ src/test/java/devutility/internal/test/service/lang/reflect/methodutils/QuietCallTest.java
@@ -0,0 +1,29 @@
+package devutility.internal.test.service.lang.reflect.methodutils;
+
+import java.lang.reflect.Method;
+
+import devutility.internal.lang.reflect.MethodUtils;
+import devutility.internal.test.BaseTest;
+import devutility.internal.test.TestExecutor;
+import devutility.internal.test.models.Gender;
+
+public class QuietCallTest extends BaseTest {
+	@Override
+	public void run() {
+		Method[] methods = Gender.class.getDeclaredMethods();
+		Method method = MethodUtils.find("parse", methods);
+
+		if (method.getParameterTypes()[0].equals(String.class)) {
+			println("Yes");
+		} else {
+			return;
+		}
+
+		Gender gender = MethodUtils.quietCall(method, "2");
+		println(gender.name());
+	}
+
+	public static void main(String[] args) {
+		TestExecutor.run(QuietCallTest.class);
+	}
+}
