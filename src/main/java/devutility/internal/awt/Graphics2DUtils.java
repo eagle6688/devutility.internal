4f43b0a2ca3ba35b3e91d099e7a2293b3aae4b33
Aldwin Su <fangyansu@163.com>
2019-03-19 00:15:41 +0800

    Update Graphics2DUtils

--- src/main/java/devutility/internal/awt/Graphics2DUtils.java
+++ src/main/java/devutility/internal/awt/Graphics2DUtils.java
@@ -1,9 +1,25 @@
 package devutility.internal.awt;
 
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 
 public class Graphics2DUtils {
-	public static void drawString(Graphics2D graphics) {
+	public static void drawString(Graphics2D graphics, Font font, Color color, AlphaComposite alphaComposite, String text, float positionX, float positionY) {
+		graphics.setFont(font);
+		graphics.setColor(color);
+		graphics.setComposite(alphaComposite);
+		graphics.drawString(text, positionX, positionY);
+	}
 
+	public static void drawString(Graphics2D graphics, Font font, Color color, float alpha, String text, float positionX, float positionY) {
+		drawString(graphics, font, color, AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha), text, positionX, positionY);
+	}
+
+	public static void rotate(Graphics2D graphics, int width, int height, int slopeAngle) {
+		if (slopeAngle > 0) {
+			graphics.rotate(Math.toRadians(slopeAngle), width / 2, height / 2);
+		}
 	}
 }
