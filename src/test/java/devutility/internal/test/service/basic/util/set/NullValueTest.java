608998478d34f682b66c86482a109e4d2981c745
Aldwin Su <fangyansu@163.com>
2019-04-17 10:54:53 +0800

    Add NullValyeTest.java

---
+++ src/test/java/devutility/internal/test/service/basic/util/set/NullValueTest.java
@@ -0,0 +1,23 @@
+package devutility.internal.test.service.basic.util.set;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import devutility.internal.test.BaseTest;
+import devutility.internal.test.TestExecutor;
+
+public class NullValueTest extends BaseTest {
+	@Override
+	public void run() {
+		Set<String> set1 = new HashSet<>();
+		set1.add(null);
+
+		Set<String> set2 = new LinkedHashSet<>();
+		set2.add(null);
+	}
+
+	public static void main(String[] args) {
+		TestExecutor.run(NullValueTest.class);
+	}
+}
