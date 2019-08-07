e436b6537051cdffb496692e5af5e9dbebf2a861
Aldwin Su <fangyansu@163.com>
2019-08-07 20:42:45 +0800

    Add AesUtils.java

---
+++ src/main/java/devutility/internal/security/AesUtils.java
@@ -0,0 +1,26 @@
+package devutility.internal.security;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+/**
+ * 
+ * AesUtils
+ * 
+ * @author: Aldwin Su
+ * @version: 2019-08-07 15:16:23
+ */
+public class AesUtils {
+	public SecretKey secretKey(String key, int keySize) throws NoSuchAlgorithmException {
+		KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+		keyGenerator.init(keySize, new SecureRandom(key.getBytes()));
+		return keyGenerator.generateKey();
+	}
+
+	public SecretKey secretKey(String key) throws NoSuchAlgorithmException {
+		return secretKey(key, 128);
+	}
+}
