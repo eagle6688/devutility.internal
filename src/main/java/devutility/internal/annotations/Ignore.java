d8debd11c6dad9d20821b8686dfe8aa5a519b1df
Aldwin Su <fangyansu@163.com>
2019-12-05 22:21:40 +0800

    Add Ignore annotation

---
+++ src/main/java/devutility/internal/annotations/Ignore.java
@@ -0,0 +1,17 @@
+package devutility.internal.annotations;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Inherited
+@Retention(RUNTIME)
+@Target({ TYPE, FIELD, METHOD })
+public @interface Ignore {
+
+}
