f4d05dd67b2c7d9399364e18c29128f55a30ea06
Aldwin Su <fangyansu@163.com>
2020-03-05 23:54:32 +0800

    Add ObjectConverter

--- src/main/java/devutility/internal/annotations/Convertor.java
+++ src/main/java/devutility/internal/annotations/Convertor.java
@@ -5,6 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 
+ * Convertor
+ * 
+ * @author: Aldwin Su
+ * @version: 2019-03-05 18:32:27
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.METHOD })
 public @interface Convertor {
