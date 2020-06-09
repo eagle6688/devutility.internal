6f58ebf758d4017d7fc7dd87ace7cd5345b30c23
Aldwin Su <fangyansu@163.com>
2020-06-09 17:51:47 +0800

    Update ExceptionUtils

--- src/main/java/devutility/internal/lang/ExceptionUtils.java
+++ src/main/java/devutility/internal/lang/ExceptionUtils.java
@@ -5,14 +5,21 @@ import java.util.List;
 
 import devutility.internal.com.SystemUtils;
 
+/**
+ * 
+ * ExceptionUtils
+ * 
+ * @author: Aldwin Su
+ * @version: 2017-08-18 23:32:34
+ */
 public class ExceptionUtils {
-	public static String toString(Exception exception) {
-		return toString(exception, SystemUtils.lineSeparator());
+	public static String toString(Throwable throwable) {
+		return toString(throwable, SystemUtils.lineSeparator());
 	}
 
-	public static String toString(Exception exception, String separator) {
+	public static String toString(Throwable throwable, String separator) {
 		StringBuilder result = new StringBuilder();
-		List<String> list = toList(exception);
+		List<String> list = toList(throwable);
 
 		for (int i = 0; i < list.size(); i++) {
 			result.append(list.get(i));
@@ -25,22 +32,22 @@ public class ExceptionUtils {
 		return result.toString();
 	}
 
-	public static List<String> toList(Exception exception) {
+	public static List<String> toList(Throwable throwable) {
 		List<String> list = new ArrayList<>();
 
-		if (exception == null) {
+		if (throwable == null) {
 			return list;
 		}
 
-		list.add(String.format("%s: %s", exception.getClass().getName(), exception.getMessage()));
-		StackTraceElement[] stackTraceElements = exception.getStackTrace();
+		list.add(String.format("%s: %s", throwable.getClass().getName(), throwable.getMessage()));
+		StackTraceElement[] stackTraceElements = throwable.getStackTrace();
 
 		if (stackTraceElements == null || stackTraceElements.length == 0) {
 			return list;
 		}
 
 		for (StackTraceElement stackTraceElement : stackTraceElements) {
-			list.add(String.format("	at %s", stackTraceElement.toString()));
+			list.add(String.format(" at %s", stackTraceElement.toString()));
 		}
 
 		return list;
