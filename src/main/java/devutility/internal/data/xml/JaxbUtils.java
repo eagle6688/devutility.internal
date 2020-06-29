dcc90dca517101e4aa11fd9ad9701781fcfdbbd6
Aldwin Su <fangyansu@163.com>
2020-06-29 15:54:01 +0800

    Overload xmlToBean in JaxbUtils.java

--- src/main/java/devutility/internal/data/xml/JaxbUtils.java
+++ src/main/java/devutility/internal/data/xml/JaxbUtils.java
@@ -16,19 +16,29 @@ import javax.xml.bind.Unmarshaller;
  */
 public class JaxbUtils {
 	/**
+	 * Convert xml Reader object to Java bean.
+	 * @param xmlReader Reader object contains xml.
+	 * @param clazz {@code Class<T> object.}
+	 * @return {@code T}
+	 * @throws JAXBException from JAXBContext.newInstance
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> T xmlToBean(Reader xmlReader, Class<T> clazz) throws JAXBException {
+		JAXBContext context = JAXBContext.newInstance(clazz);
+		Unmarshaller unmarshaller = context.createUnmarshaller();
+		return (T) unmarshaller.unmarshal(xmlReader);
+	}
+
+	/**
 	 * Convert xml string to Bean.
 	 * @param xmlContent Xml content.
 	 * @param clazz {@code Class<T> object.}
 	 * @return {@code T}
 	 * @throws JAXBException from JAXBContext.newInstance
 	 */
-	@SuppressWarnings("unchecked")
 	public static <T> T xmlToBean(String xmlContent, Class<T> clazz) throws JAXBException {
-		JAXBContext context = JAXBContext.newInstance(clazz);
-		Unmarshaller unmarshaller = context.createUnmarshaller();
-
 		try (Reader reader = new StringReader(xmlContent)) {
-			return (T) unmarshaller.unmarshal(reader);
+			return xmlToBean(reader, clazz);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
