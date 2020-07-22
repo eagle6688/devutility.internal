d265e0aff6fdd75a0a2936fed6be27ba8e3ea434
Aldwin Su <fangyansu@163.com>
2020-07-23 00:02:48 +0800

    Update

--- src/main/java/devutility/internal/model/OperationResult.java
+++ src/main/java/devutility/internal/model/OperationResult.java
@@ -1,5 +1,12 @@
 package devutility.internal.model;
 
+/**
+ * 
+ * OperationResult
+ * 
+ * @author: Aldwin Su
+ * @version: 2017-12-01 18:46:09
+ */
 public class OperationResult extends BaseResponse<Object> {
 	public OperationResult(boolean succeeded, String message, Object data, int code) {
 		super(succeeded, message, data, code);
