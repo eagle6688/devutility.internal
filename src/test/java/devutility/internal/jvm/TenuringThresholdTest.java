552559d277a1d636b003ff37d4f26d952db66640
Aldwin Su <fangyansu@163.com>
2021-04-23 21:52:57 +0800

    Add test case for JVM

---
+++ src/test/java/devutility/internal/jvm/TenuringThresholdTest.java
@@ -0,0 +1,15 @@
+package devutility.internal.jvm;
+
+public class TenuringThresholdTest {
+	private static final int _1MB = 1024 * 1024;
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		byte[] allocation1, allocation2, allocation3;
+		allocation1 = new byte[_1MB / 4];
+		allocation2 = new byte[4 * _1MB];
+		allocation3 = new byte[4 * _1MB];
+		allocation3 = null;
+		allocation3 = new byte[4 * _1MB];
+	}
+}
