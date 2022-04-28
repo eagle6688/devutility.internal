a37df7f39d62347edcfc6621e963a5596075a376
Aldwin Su <fangyansu@163.com>
2022-04-28 11:11:55 +0800

    Update test case

--- src/test/java/devutility/internal/basic/lang/integer/LongToBinaryStringTest.java
+++ src/test/java/devutility/internal/basic/lang/integer/LongToBinaryStringTest.java
@@ -6,14 +6,22 @@ import devutility.internal.test.TestExecutor;
 public class LongToBinaryStringTest extends BaseTest {
 	@Override
 	public void run() {
-		toBinaryString(4159L);
-		toBinaryString(137472512063L);
-		toBinaryString(137438957631L);
+		toBinaryString(4159L, false);
+		toBinaryString(137472512063L, false);
+		toBinaryString(26388303753279L, true);
 	}
 
-	void toBinaryString(long value) {
+	void toBinaryString(long value, boolean position) {
 		String str = Long.toBinaryString(value);
 		System.out.println(String.format("Value: %d, Binary: %s", value, str));
+
+		if (!position) {
+			return;
+		}
+		
+		for (int i = 0; i < str.length(); i++) {
+			System.out.println(String.format("Right index %d, value %c", i + 1, str.charAt(i)));
+		}
 	}
 
 	public static void main(String[] args) {
